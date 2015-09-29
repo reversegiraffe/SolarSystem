e47b87e5961fc848fab591627febfff562a95b2e
Caleb Hicks <calebhicks@gmail.com>
2015-09-29 15:31:53 -0600

    adds sample solution

---
+++ SolarSystem/PlanetDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PlanetDetailViewController.swift
+//  SolarSystem
+//
+//  Created by Caleb Hicks on 9/28/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class PlanetDetailViewController: UIViewController {
+
+    @IBOutlet weak var planetImageView: UIImageView!
+    
+    @IBOutlet weak var diameterLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var lengthLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func updateWithPlanet(planet: Planet) {
+        title = planet.name
+        planetImageView.image = UIImage(named: planet.imageName)
+        diameterLabel.text = "\(planet.diameter)"
+        distanceLabel.text = "\(planet.millionKMsFromSun) 10^6km"
+        lengthLabel.text = "\(planet.dayLength) hours"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
